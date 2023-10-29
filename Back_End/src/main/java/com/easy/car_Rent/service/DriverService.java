a4c4d69c9a4023c81fdb1f9982bfa9f7068e8e6b
nimeshaMadubashini <nimeshamadubashini040@gmail.com>
2023-10-30 00:12:15 +0530

    update driver js and backend

--- Back_End/src/main/java/com/easy/car_Rent/service/DriverService.java
+++ Back_End/src/main/java/com/easy/car_Rent/service/DriverService.java
@@ -1,4 +1,20 @@
 package com.easy.car_Rent.service;
 
+import com.easy.car_Rent.dto.CustomDTO;
+import com.easy.car_Rent.dto.DriverDTO;
+import com.easy.car_Rent.entity.Driver;
+
+import java.util.ArrayList;
+
 public interface DriverService {
+    void saveDriver(DriverDTO dto);
+    void updateDriver(DriverDTO dto);
+    void deleteDriver(String reg_ID);
+    ArrayList<DriverDTO> getAllDriver();
+    ArrayList<DriverDTO> getAllAvailabilityDriver();
+    CustomDTO userIdGenerate();
+    CustomDTO getSumAvailableDriver();
+    CustomDTO getSumUnavailableDriver();
+    Driver searchDriverId(String id);
+    CustomDTO getSumDriver();
 }
