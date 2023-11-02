315ef25bd055040d3ce6fac9d5230d501f834407
nimeshaMadubashini <nimeshamadubashini040@gmail.com>
2023-11-02 15:28:58 +0530

    correct issue with dto

---
+++ Back_End/src/main/java/com/easy/car_Rent/dto/DriverScheduleDTO.java
@@ -0,0 +1,18 @@
+package com.easy.car_Rent.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+
+public class DriverScheduleDTO {
+    private String driverID;
+    private String rentID;
+
+}
